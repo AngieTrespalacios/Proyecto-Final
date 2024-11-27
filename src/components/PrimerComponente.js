123c93b7aceb7776de342ecee5454f53f7b58f1e
AngieTrespalacios <angiet.esquivel@gmail.com>
2024-11-27 08:19:30 -0500

    Descripción breve de los cambios

--- src/components/PrimerComponente.js
+++ src/components/PrimerComponente.js
@@ -4,7 +4,6 @@ const PrimerComponente = () => {
   return (
     <div style={{ textAlign: 'center', color: '#000', backgroundColor: '#fff', padding: '20px' }}>
       <h1 style={{ color: '#d90429' }}>Proyecto Final</h1>
-
       <p>Autores:</p>
       <p>Gamero Yuleicy</p>
       <p>Suárez Isabella</p>
