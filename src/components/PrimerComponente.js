41609a2b9ffc9a6bcea84bb114a03b5e6f29c036
AngieTrespalacios <angiet.esquivel@gmail.com>
2024-11-27 07:53:11 -0500

    Se elimino una linea en el primer componente

--- src/components/PrimerComponente.js
+++ src/components/PrimerComponente.js
@@ -4,7 +4,7 @@ const PrimerComponente = () => {
   return (
     <div style={{ textAlign: 'center', color: '#000', backgroundColor: '#fff', padding: '20px' }}>
       <h1 style={{ color: '#d90429' }}>Proyecto Final</h1>
-      <p>H UJUETA SAS</p>
+
       <p>Autores:</p>
       <p>Gamero Yuleicy</p>
       <p>Suárez Isabella</p>
